e3cb382c6845995f362908466847f6754e59751d
bhanu <unahb.sharma@gmail.com>
2017-07-21 15:02:10 +0530

    errors resolved

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # File to introduce constants
 
-APP_ACCESS_TOKEN='2212744965.50ac7df.7decd15884564bbab29fe4313042bc0a'          # Access token genrated from developer account
+APP_ACCESS_TOKEN='3680765130.197733b.e08721b9340e47fcb6e04c524708cdbe'          # Access token genrated from developer account
 BASE_URL='https://api.instagram.com/v1/'            # Base URl
 insta_username = ""
